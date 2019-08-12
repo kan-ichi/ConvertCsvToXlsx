a12b72e4b34951e42b5eee25f0d6281230d76c9f
kan-ichi <kty023@gmail.com>
2019-08-12 18:14:03 +0900

    upload source code

---
+++ ConvertCsvToXlsx/Program.cs
@@ -0,0 +1,242 @@
+﻿using ClosedXML.Excel;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConvertCsvToXlsx
+{
+    class Program
+    {
+        #region エントリーポイント
+
+        /// <summary>
+        /// エントリーポイント
+        /// </summary>
+        public static void Main(string[] _args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            Request request = new Request();
+
+            request.InputCsvFolderName = ConfigurationManager.AppSettings["InputCsvFolderName"];
+            request.InputCsvEncoding = ConfigurationManager.AppSettings["InputCsvEncoding"];
+            request.InputCsvSkipFirstRow = (ConfigurationManager.AppSettings["InputCsvSkipFirstRow"] == "Yes");
+            request.OutputXlsxFolderName = ConfigurationManager.AppSettings["OutputXlsxFolderName"];
+            request.OutputXlsxFileName = ConfigurationManager.AppSettings["OutputXlsxFileName"];
+            request.OutputXlsxForceAllCellTypeAsString = (ConfigurationManager.AppSettings["OutputXlsxForceAllCellTypeAsString"] == "Yes");
+
+            request.InputCsvFileNameByArgs = null;
+            request.OutputXlsxFileNameByArgs = null;
+
+            if (_args.Length >= 1)
+            {
+                request.InputCsvFileNameByArgs = _args[0];
+            }
+
+            if (_args.Length >= 2)
+            {
+                request.OutputXlsxFileNameByArgs = _args[1];
+            }
+
+            MainProcess(request);
+        }
+
+        #endregion
+
+        #region リクエスト変数
+
+        /// <summary>
+        /// リクエスト変数
+        /// </summary>
+        public struct Request
+        {
+            /// <summary>
+            /// 入力CSVフォルダ名
+            /// </summary>
+            public string InputCsvFolderName { get; set; }
+
+            /// <summary>
+            /// 入力CSVエンコーディング
+            /// </summary>
+            public string InputCsvEncoding { get; set; }
+
+            /// <summary>
+            /// 入力CSVの最初の一行を読込対象外とするか？
+            /// </summary>
+            public bool InputCsvSkipFirstRow { get; set; }
+
+            /// <summary>
+            /// 出力xlsxフォルダ名
+            /// </summary>
+            public string OutputXlsxFolderName { get; set; }
+
+            /// <summary>
+            /// 出力xlsxファイル名
+            /// </summary>
+            public string OutputXlsxFileName { get; set; }
+
+            /// <summary>
+            /// 出力xlsxのすべてのセルの属性を文字列に設定するか？
+            /// </summary>
+            public bool OutputXlsxForceAllCellTypeAsString { get; set; }
+
+            /// <summary>
+            /// コマンドライン引数で指定された入力CSVファイル名
+            /// </summary>
+            public string InputCsvFileNameByArgs { get; set; }
+
+            /// <summary>
+            /// コマンドライン引数で指定された出力xlsxファイル名
+            /// </summary>
+            public string OutputXlsxFileNameByArgs { get; set; }
+        }
+
+        #endregion
+
+        #region メイン処理
+
+        /// <summary>
+        /// メイン処理
+        /// </summary>
+        private static void MainProcess(Request _request)
+        {
+            WriteConsoleLogMessage("CSV -> xlsx 変換処理を開始します");
+
+            // 処理する入力ファイルのリストを作成
+            // コマンドライン引数で入力ファイル名が指定されていないならば、
+            // アプリケーション構成ファイル定義のフォルダ内のすべてのファイルを処理
+            // 入力ファイル名がコマンドライン引数で指定されているのならば、そのファイルだけを処理
+            FileInfo[] inputFiles;
+            if (string.IsNullOrEmpty(_request.InputCsvFileNameByArgs))
+            {
+                DirectoryInfo di = new DirectoryInfo(_request.InputCsvFolderName);
+                inputFiles = di.GetFiles("*.csv", System.IO.SearchOption.TopDirectoryOnly);
+                Array.Sort<FileInfo>(inputFiles, delegate (FileInfo a, FileInfo b)
+                {
+                    return a.Name.CompareTo(b.Name);
+                });
+            }
+            else
+            {
+                inputFiles = new FileInfo[1] { new FileInfo(_request.InputCsvFileNameByArgs) };
+            }
+
+            // 出力ファイルについて
+            // 引数で出力ファイル名が指定されていれば、それを使用
+            // 指定されていないのならば、アプリケーション構成ファイル定義の値を使用
+            string outputXlsxFolderAndFileName;
+            outputXlsxFolderAndFileName = _request.OutputXlsxFileNameByArgs ?? Path.Combine(_request.OutputXlsxFolderName, _request.OutputXlsxFileName);
+
+            //  出力xlsxを作成し、シートを追加
+            var book = new XLWorkbook();
+            var sheet = book.Worksheets.Add(Left(Path.GetFileName(outputXlsxFolderAndFileName), 30));
+            int outputRowNumber = 1;
+
+            // 入力ファイルを読み、出力用xlsxに値を設定する
+            foreach (var inputFile in inputFiles)
+            {
+                if (inputFile.Extension.ToLower() != ".csv") continue; // 「.csv2」などの拡張子が検索されるが、それは処理対象外とする
+                WriteConsoleLogMessage("読み込んでいます " + inputFile.FullName);
+
+                var parser = new TextFieldParser(inputFile.FullName, Encoding.GetEncoding(_request.InputCsvEncoding));
+                using (parser)
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(","); // カンマ区切り
+
+                    parser.HasFieldsEnclosedInQuotes = true; // フィールドが引用符で囲まれているか
+                    parser.TrimWhiteSpace = false;           // フィールドの空白トリム設定
+
+                    int inputLineNumber = 0;
+                    while (!parser.EndOfData) // ファイルの終端までループ
+                    {
+                        string[] row = parser.ReadFields(); // フィールドを読込
+                        inputLineNumber++;
+                        if (_request.InputCsvSkipFirstRow && inputLineNumber == 1) continue; // 最初の一行を読込対象外としている場合、読み飛ばし
+
+                        for (int i = 0; i < row.Length; i++)
+                        {
+                            var cell = sheet.Cell(outputRowNumber, i + 1);
+                            string cellStringValue = Convert.ToString(row[i]);
+
+                            if (_request.OutputXlsxForceAllCellTypeAsString)
+                            {
+                                cell.SetValue<string>(cellStringValue).Style.NumberFormat.SetFormat("@");
+                                continue;
+                            }
+
+                            DateTime dateTimeCellValue;
+                            if (DateTime.TryParse(cellStringValue, out dateTimeCellValue))
+                            {
+                                TimeSpan timeSpanCellValue;
+                                if (TimeSpan.TryParse(cellStringValue, out timeSpanCellValue))
+                                {
+                                    cell.SetValue<TimeSpan>(timeSpanCellValue);
+                                }
+                                else
+                                {
+                                    cell.SetValue<DateTime>(dateTimeCellValue);
+                                }
+                                continue;
+                            }
+
+                            bool boolCellValue;
+                            if (bool.TryParse(cellStringValue, out boolCellValue))
+                            {
+                                cell.SetValue<bool>(boolCellValue);
+                                continue;
+                            }
+
+                            decimal decimalCellValue;
+                            if (decimal.TryParse(cellStringValue, out decimalCellValue))
+                            {
+                                cell.SetValue<decimal>(decimalCellValue);
+                                continue;
+                            }
+
+                            cell.SetValue<string>(cellStringValue);
+                        }
+
+                        outputRowNumber++;
+                    }
+                }
+            }
+
+            // 出力用xlsxを書き込む
+            WriteConsoleLogMessage("作成しています " + outputXlsxFolderAndFileName);
+            book.SaveAs(outputXlsxFolderAndFileName);
+
+            WriteConsoleLogMessage("CSV -> xlsx 変換処理を終了します");
+        }
+
+        #endregion
+
+        #region プライベートメソッド
+
+        /// <summary>
+        /// コンソールにメッセージを出力します
+        /// </summary>
+        private static void WriteConsoleLogMessage(string _message)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + " " + _message);
+        }
+
+        /// <summary>
+        /// 文字列の左端から指定された文字数分の文字列を返します
+        /// </summary>
+        private static string Left(string _target, int _length)
+        {
+            if (_length <= _target.Length)
+            {
+                return _target.Substring(0, _length);
+            }
+            return _target;
+        }
+
+        #endregion
+    }
+}
---
+++ ConvertCsvToXlsxTest/ProgramTest.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.IO;
+using ClosedXML.Excel;
+using System.Text;
+
+namespace ConvertCsvToXlsxTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        /// <summary>
+        /// メイン処理のテスト
+        /// ・入力CSVのファイル名が指定されていない場合、入力CSVのフォルダのすべてのファイルが処理対象となる
+        /// ・入力ファイルの拡張子が「csv」でない場合、読み飛ばされる
+        /// ・出力xlsxのすべてのセルの属性を文字列に設定する
+        /// ・入力CSVと出力xlsxの内容が一致する
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testMethodName = MethodBase.GetCurrentMethod().Name;
+            string testingFolderName = Path.Combine(GetAppPath(), testMethodName);
+            Directory.CreateDirectory(testingFolderName);
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+
+            string inputCsvPathX;
+            {
+                string inputCsvFileName = testMethodName + ".csvX";
+                inputCsvPathX = Path.Combine(testingFolderName, inputCsvFileName);
+                string[] lines = new string[] { };
+                File.WriteAllLines(inputCsvPathX, lines, enc);
+            }
+
+            string inputCsvPath;
+            {
+                string inputCsvFileName = testMethodName + ".CSV";
+                inputCsvPath = Path.Combine(testingFolderName, inputCsvFileName);
+                string[] lines = new string[] {
+                    "1a,\"1,b\",1c",
+                    "2a,,2c,2d",
+                    "3a, "
+                };
+                File.WriteAllLines(inputCsvPath, lines, enc);
+            }
+
+            string outputXlsxPath;
+            {
+                string outputXlsxFileName = testMethodName + ".xlsx";
+                outputXlsxPath = Path.Combine(testingFolderName, outputXlsxFileName);
+            }
+
+            ConvertCsvToXlsx.Program.Request request = new ConvertCsvToXlsx.Program.Request();
+            {
+                request.InputCsvFolderName = testingFolderName;
+                request.InputCsvEncoding = enc.WebName;
+                request.OutputXlsxFileNameByArgs = outputXlsxPath;
+                request.OutputXlsxForceAllCellTypeAsString = true;
+            }
+
+            {
+                var target = new PrivateType(typeof(ConvertCsvToXlsx.Program));
+                target.InvokeStatic("MainProcess", request);
+            }
+
+            using (var workbook = new XLWorkbook(outputXlsxPath))
+            {
+                string sheetName = Path.GetFileName(outputXlsxPath);
+                var worksheet = workbook.Worksheet(sheetName);
+                var outputXlsxTable = worksheet.RangeUsed().AsTable();
+
+                Assert.AreEqual(3, outputXlsxTable.RowCount());
+                Assert.AreEqual("1a", worksheet.Cell(1, 1).Value);
+                Assert.AreEqual("1,b", worksheet.Cell(1, 2).Value);
+                Assert.AreEqual("1c", worksheet.Cell(1, 3).Value);
+                Assert.AreEqual("2a", worksheet.Cell(2, 1).Value);
+                Assert.AreEqual("", worksheet.Cell(2, 2).Value);
+                Assert.AreEqual("2c", worksheet.Cell(2, 3).Value);
+                Assert.AreEqual("3a", worksheet.Cell(3, 1).Value);
+                Assert.AreEqual(" ", worksheet.Cell(3, 2).Value);
+                Assert.AreEqual("", worksheet.Cell(3, 3).Value);
+            }
+
+            File.Delete(inputCsvPathX);
+            File.Delete(inputCsvPath);
+            File.Delete(outputXlsxPath);
+            Directory.Delete(testingFolderName);
+        }
+
+        /// <summary>
+        /// エントリーポイントからメイン処理のテスト
+        /// ・入力CSVのレコードがない場合、出力xlsxのセルには何も出力されない（使用中のセル範囲がnullとなる）
+        /// ・出力xlsxのシート名は出力xlsxのファイル名であるが、先頭30文字のみ使用される
+        /// </summary>
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string testMethodName = MethodBase.GetCurrentMethod().Name;
+            string testingFolderName = GetAppPath();
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+
+            string inputCsvPath;
+            {
+                string inputCsvFileName = testMethodName + ".csv";
+                inputCsvPath = Path.Combine(testingFolderName, inputCsvFileName);
+                string[] lines = new string[] { };
+                File.WriteAllLines(inputCsvPath, lines, enc);
+            }
+
+            string outputXlsxFileName = testMethodName + "123456789012345678901234567890" + ".xlsx";
+            string outputXlsxPath;
+            {
+                outputXlsxPath = Path.Combine(testingFolderName, outputXlsxFileName);
+            }
+
+            {
+                string[] args = new string[] { inputCsvPath, outputXlsxPath };
+                ConvertCsvToXlsx.Program.Main(args);
+            }
+
+            using (var workbook = new XLWorkbook(outputXlsxPath))
+            {
+                string sheetName = outputXlsxFileName.Substring(0, 30);
+                var worksheet = workbook.Worksheet(sheetName);
+                Assert.IsNull(worksheet.RangeUsed());
+            }
+
+            File.Delete(inputCsvPath);
+            File.Delete(outputXlsxPath);
+        }
+
+        /// <summary>
+        /// エントリーポイントのテスト
+        /// ・入力ファイル名がブランクであるため、例外発生
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestMethod3()
+        {
+            string[] args = new string[] { };
+            ConvertCsvToXlsx.Program.Main(args);
+        }
+
+        /// <summary>
+        /// メイン処理のテスト
+        /// ・入力CSVの最初の一行を読込対象外とした場合、最初の一行は読込対象外となる
+        /// </summary>
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string testMethodName = MethodBase.GetCurrentMethod().Name;
+            string testingFolderName = GetAppPath();
+            Encoding enc = Encoding.GetEncoding("utf-8");
+
+            string inputCsvPath;
+            {
+                string inputCsvFileName = testMethodName + ".CSV";
+                inputCsvPath = Path.Combine(testingFolderName, inputCsvFileName);
+                string[] lines = new string[] {
+                    "1a,\"1,b\",1c",
+                    "2a,,2c,2d",
+                    "3a, "
+                };
+                File.WriteAllLines(inputCsvPath, lines, enc);
+            }
+
+            string outputXlsxPath;
+            {
+                string outputXlsxFileName = testMethodName + ".xlsx";
+                outputXlsxPath = Path.Combine(testingFolderName, outputXlsxFileName);
+            }
+
+            ConvertCsvToXlsx.Program.Request request = new ConvertCsvToXlsx.Program.Request();
+            {
+                request.InputCsvFileNameByArgs = inputCsvPath;
+                request.InputCsvEncoding = enc.WebName;
+                request.InputCsvSkipFirstRow = true;
+                request.OutputXlsxFileNameByArgs = outputXlsxPath;
+            }
+
+            {
+                var target = new PrivateType(typeof(ConvertCsvToXlsx.Program));
+                target.InvokeStatic("MainProcess", request);
+            }
+
+            using (var workbook = new XLWorkbook(outputXlsxPath))
+            {
+                string sheetName = Path.GetFileName(outputXlsxPath);
+                var worksheet = workbook.Worksheet(sheetName);
+                var outputXlsxTable = worksheet.RangeUsed().AsTable();
+
+                Assert.AreEqual(2, outputXlsxTable.RowCount());
+                Assert.AreEqual("2a", worksheet.Cell(1, 1).Value);
+                Assert.AreEqual("", worksheet.Cell(1, 2).Value);
+                Assert.AreEqual("2c", worksheet.Cell(1, 3).Value);
+                Assert.AreEqual("3a", worksheet.Cell(2, 1).Value);
+                Assert.AreEqual(" ", worksheet.Cell(2, 2).Value);
+                Assert.AreEqual("", worksheet.Cell(2, 3).Value);
+            }
+
+            File.Delete(inputCsvPath);
+            File.Delete(outputXlsxPath);
+        }
+
+        /// <summary>
+        /// メイン処理のテスト
+        /// ・CSVの文字列をデータ変換する処理のテスト
+        /// ・時刻 -> 日付時刻 -> 日付 -> 真偽値 -> 数値 -> 文字列 の順に判定する
+        /// </summary>
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string testMethodName = MethodBase.GetCurrentMethod().Name;
+            string testingFolderName = GetAppPath();
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+
+            string inputCsvPath;
+            {
+                string inputCsvFileName = testMethodName + ".CSV";
+                inputCsvPath = Path.Combine(testingFolderName, inputCsvFileName);
+                string[] lines = new string[] {
+                    "12:34:56           , 0:0         , 23:59:59           , -00:00, 24:00",
+                    "2000/02/29         , 1900/1/1    , 2999/12/31         , 2000-01-01, 1999/02/29",
+                    "2000/02/29 12:34:56, 1900/1/1 0:0, 2999/12/31 23:59:59, 2000/01/01 00:00:60",
+                    "True               , false       , TRUE               , falsE, TRUE/FALSE",
+                    "-1.2345678901234567, -0          , 123456789012345678 , １２３４５６７８"
+                };
+                File.WriteAllLines(inputCsvPath, lines, enc);
+            }
+
+            string outputXlsxPath;
+            {
+                string outputXlsxFileName = testMethodName + ".xlsx";
+                outputXlsxPath = Path.Combine(testingFolderName, outputXlsxFileName);
+            }
+
+            ConvertCsvToXlsx.Program.Request request = new ConvertCsvToXlsx.Program.Request();
+            {
+                request.InputCsvFileNameByArgs = inputCsvPath;
+                request.InputCsvEncoding = enc.WebName;
+                request.OutputXlsxFileNameByArgs = outputXlsxPath;
+            }
+
+            {
+                var target = new PrivateType(typeof(ConvertCsvToXlsx.Program));
+                target.InvokeStatic("MainProcess", request);
+            }
+
+            using (var workbook = new XLWorkbook(outputXlsxPath))
+            {
+                string sheetName = Path.GetFileName(outputXlsxPath);
+                var worksheet = workbook.Worksheet(sheetName);
+                var outputXlsxTable = worksheet.RangeUsed().AsTable();
+
+                Assert.AreEqual(new TimeSpan(12, 34, 56), TimeSpan.FromDays(worksheet.Cell(1, 1).GetValue<double>()));
+                Assert.AreEqual(new TimeSpan(00, 00, 00), TimeSpan.FromDays(worksheet.Cell(1, 2).GetValue<double>()));
+                Assert.AreEqual(new TimeSpan(23, 59, 59), TimeSpan.FromDays(worksheet.Cell(1, 3).GetValue<double>()));
+                Assert.AreEqual(" -00:00", worksheet.Cell(1, 4).GetValue<string>());
+                Assert.AreEqual(" 24:00", worksheet.Cell(1, 5).GetValue<string>());
+
+                Assert.AreEqual(new DateTime(2000, 02, 29), worksheet.Cell(2, 1).GetValue<DateTime>());
+                Assert.AreEqual(new DateTime(1900, 01, 01), worksheet.Cell(2, 2).GetValue<DateTime>());
+                Assert.AreEqual(new DateTime(2999, 12, 31), worksheet.Cell(2, 3).GetValue<DateTime>());
+                Assert.AreEqual(new DateTime(2000, 01, 01), worksheet.Cell(2, 4).GetValue<DateTime>());
+                Assert.AreEqual(" 1999/02/29", worksheet.Cell(2, 5).GetValue<string>());
+
+                Assert.AreEqual(new DateTime(2000, 02, 29, 12, 34, 56), worksheet.Cell(3, 1).GetValue<DateTime>());
+                Assert.AreEqual(new DateTime(1900, 01, 01, 00, 00, 00), worksheet.Cell(3, 2).GetValue<DateTime>());
+                Assert.AreEqual(new DateTime(2999, 12, 31, 23, 59, 59), worksheet.Cell(3, 3).GetValue<DateTime>());
+                Assert.AreEqual(" 2000/01/01 00:00:60", worksheet.Cell(3, 4).GetValue<string>());
+
+                Assert.AreEqual(true, worksheet.Cell(4, 1).GetValue<bool>());
+                Assert.AreEqual(false, worksheet.Cell(4, 2).GetValue<bool>());
+                Assert.AreEqual(true, worksheet.Cell(4, 3).GetValue<bool>());
+                Assert.AreEqual(false, worksheet.Cell(4, 4).GetValue<bool>());
+                Assert.AreEqual(" TRUE/FALSE", worksheet.Cell(4, 5).GetValue<string>());
+
+                Assert.AreEqual(-1.2345678901234600m, worksheet.Cell(5, 1).GetValue<decimal>()); // Excelの仕様で15桁目以降に誤差が生じる
+                Assert.AreEqual(0m, worksheet.Cell(5, 2).GetValue<decimal>());
+                Assert.AreEqual(123456789012346000m, worksheet.Cell(5, 3).GetValue<decimal>()); // Excelの仕様で15桁目以降に誤差が生じる
+                Assert.AreEqual(" １２３４５６７８", worksheet.Cell(5, 4).GetValue<string>());
+            }
+
+            File.Delete(inputCsvPath);
+            File.Delete(outputXlsxPath);
+        }
+
+        /// <summary>
+        /// テスト実施中のフォルダパスを取得します
+        /// </summary>
+        private static string GetAppPath()
+        {
+            string path = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+            //URIを通常のパス形式に変換する
+            Uri u = new Uri(path);
+            path = u.LocalPath + Uri.UnescapeDataString(u.Fragment);
+            return System.IO.Path.GetDirectoryName(path);
+        }
+
+    }
+}
